0c80c15f8809e278981eaa69792d3a80198fddf3
barnesbrooke <89588661+barnesbrooke@users.noreply.github.com>
2021-10-21 15:11:23 -0400

    Create endAssignment3.m

---
+++ assignment3/endAssignment3.m
@@ -0,0 +1,6 @@
+function [message] = endAssignment3
+%Question 10 
+%   This function takes no input and displays a message.
+    message = fprintf ('Thank goodness this assignment is over!');
+end
+
