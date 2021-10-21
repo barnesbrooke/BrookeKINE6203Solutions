9f6657aa912b27260a32cf982bcd5e8707750987
barnesbrooke <89588661+barnesbrooke@users.noreply.github.com>
2021-10-21 15:11:26 -0400

    Create counting.m

---
+++ assignment3/counting.m
@@ -0,0 +1,8 @@
+function [result] = counting(number1,number2)
+%QUestion 8
+%   This function takes two numbers as input and displays a counter from
+%   the lower number to the higher number.
+for i = number1:number2
+    disp(i);
+end
+
